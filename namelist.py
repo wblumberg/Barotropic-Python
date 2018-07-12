c24e33e48b6164277219633922595bc4dd3e60d4
Greg Blumberg <wblumberg@ou.edu>
2018-07-12 17:18:36 -0500

    Added more namelist parameters.

--- namelist.py
+++ namelist.py
@@ -15,6 +15,13 @@ smooth_topo = 1            # Smooth the topography by using a Guassian filter
 integration_method = 'rk4'  # Integration method ('leapfrog', 'rk4')
 fluid_height = 10000         # Fluid height (m).
 
+# Idealized Initial Conditions (for idealized flow...future models will allow for realistic initial conditions)
+mag = 0 # Max base state zonal wind speed.
+A = 1.5 * 8e-5 # The vorticity perturbation of the flow (set to 0 for no perturbation).
+m = 1 # Zonal wavenumber around the sphere for the perturbation.
+theta0 = 45 # Center latitude of the sinusoidal vorticity perturbation
+thetaW = 15 # Latitudinal width of the vorticity perturbation
+
 # Extra vorticity forcing
 use_forcing = False         # Add a synthetic hurricane or extra forcing to the model.
 forcing_lat = 10            # Latitude center of the forcing
@@ -34,3 +41,4 @@ fourier_inc = 1            # Fourier increment for computing dampening eddy spon
 # Constants
 Re = 6378100.              # Radius of earth (m)
 omega = 7.292E-5           # Earth's angular momentum (s^-1)
+g = 9.81                   # Gravity (m s^-2)
