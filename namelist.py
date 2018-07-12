b99be5a347004bc1130f8c98fc1d316da29b96e9
Greg Blumberg <wblumberg@ou.edu>
2018-07-12 16:57:17 -0500

    Removed some namelist options.

--- namelist.py
+++ namelist.py
@@ -5,21 +5,25 @@ This module contains the variables used to run barotropic_spectral.py
 import os
     
 # Integration options
-dt = 300                 # Timestep (seconds)
-ntimes = 1360               # Number of time steps to integrate
+dt = 200                 # Timestep (seconds)
+ntimes = 1060               # Number of time steps to integrate
 plot_freq = 6              # Frequency of output plots in hours (if 0, no plots are made)
 M = None                   # Truncation (if None, defaults to # latitudes)
 r = 0.2                    # Coefficient for Robert Filter
-topo = 'isolated_mountain'              # Topography (Earth, Mars, flat)
-#topo = 'flat'
-#topo = 'block'
+topo = 'isolated_mountain' #'isolated_mountain'   # Topography (Earth, Mars, flat, isolated_mountain, block)
 smooth_topo = 1            # Smooth the topography by using a Guassian filter
-integration_method = 'leapfrog'
-integration_method = 'rk4'
+integration_method = 'rk4'  # Integration method ('leapfrog', 'rk4')
+fluid_height = 10000         # Fluid height (m).
+
+# Extra vorticity forcing
+use_forcing = False         # Add a synthetic hurricane or extra forcing to the model.
+forcing_lat = 10            # Latitude center of the forcing
+forcing_lon = 180           # Longitude center of the forcing
+forcing_amp = 10e-10        # Amplitude of the forcing (s^-2)
+forcing_time = 700         # Number of seconds to apply the forcing
 
 # I/O parameters
 figdir = os.path.join(os.getcwd(), 'figures')  # Figure directory
-
 
 # Diffusion parameters
 diff_opt = 'des'           # Hyperdiffusion option ('off' = none, 'del4' = del^4, 'des' = DES)
@@ -27,10 +31,6 @@ k = 2.338e16               # Diffusion coefficient for del^4 hyperdiffusion (dif
 nu = 1E-4                  # Dampening coefficient for DES hyperdiffusion (diff_opt='des')
 fourier_inc = 1            # Fourier increment for computing dampening eddy sponge (diff_opt='des')
 
-
 # Constants
 Re = 6378100.              # Radius of earth (m)
 omega = 7.292E-5           # Earth's angular momentum (s^-1)
-g = 9.81                   # Gravitational acceleration (m s^-2)
-Rd = 287.                  # Dry air gas constant
-Ts = 300.                  # Surface tempreature? K 
